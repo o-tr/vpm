fd1fe8a276764095f862effb01947d7ab824227f
ootr <git@ootr.jp>
2024-08-17 01:33:03 +0900

    デバッグ

--- utils/build.ts
+++ utils/build.ts
@@ -27,6 +27,7 @@ void (async()=>{
         "Authorization": `token ${token}`
       }
     }).then(res => res.json());
+    console.log(releases);
     const tags = releases.map((release: {tag_name: string}) => release.tag_name);
     const manifests = await Promise.all(tags.map(async (tag) => {
       const manifest = await fetch(`https://raw.githubusercontent.com/${pkg}/${tag}/package.json`).then(res => res.json());
