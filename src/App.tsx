f1bf7813b622f9b96112d1db3b04bca6da2506cf
ootr <git@ootr.jp>
2024-09-04 05:02:39 +0900

    過去バージョンを折りたたむように変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import './App.css'
-import { useManifests } from './hooks/useManifests'
+import { useState } from 'react';
+import './App.css';
+import { useManifests } from './hooks/useManifests';
+import { Manifest, PackageItem } from './types/manifest';
 
 function App() {
   const manifests = useManifests();
@@ -24,31 +26,69 @@ function App() {
               <span>Unity: {latestVersion.unity}</span>
               <span>License: {latestVersion.license}</span>
             </div>
-            <table className='versions'>
-              <tr>
-                <th>version</th>
-                <th>dependencies</th>
-                <th>link</th>
-              </tr>
-              {versions.map(([version, packageItem]) => {
-                return <tr key={version} className='version'>
-                  <td>{packageItem.version}</td>
-                  <td className='list'>
-                    {Object.entries(packageItem.vpmDependencies).map(([packageName, version]) => {
-                      if (manifests.packages[packageName]) {
-                        return <a key={packageName} href={`#${packageName}`}>{packageName}@{version}</a>
-                      }
-                      return <span key={packageName}>{packageName}@{version}</span>
-                    })}
-                  </td>
-                  <td><a href={packageItem.url}>Download</a></td>
-                </tr>
-              })}
-            </table>
+            <VersionsDisplay manifests={manifests} versions={versions} />
           </div>
         })}
       </div>
     </>
+  )
+}
+
+const VersionsDisplay = ({ manifests,versions }: { manifests: Manifest,versions: [string, PackageItem][] }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  if (!isExpanded){
+    const packageItem = versions[0][1];
+    return (
+      <table className='versions'>
+        <tr>
+          <th>version</th>
+          <th>dependencies</th>
+          <th>link</th>
+        </tr>
+        <tr className='version'>
+          <td>{packageItem.version}</td>
+          <td className='list'>
+            {Object.entries(packageItem.vpmDependencies).map(([packageName, version]) => {
+              if (manifests.packages[packageName]) {
+                return <a key={packageName} href={`#${packageName}`}>{packageName}@{version}</a>
+              }
+              return <span key={packageName}>{packageName}@{version}</span>
+            })}
+          </td>
+          <td><a href={packageItem.url}>Download</a></td>
+        </tr>
+        <tr className='expand'>
+          <td colSpan={3} onClick={() => setIsExpanded(true)}>
+            &lt;&lt; Show all versions &gt;&gt;
+          </td>
+        </tr>
+      </table>
+    )
+  }
+
+  return (
+    <table className='versions'>
+      <tr>
+        <th>version</th>
+        <th>dependencies</th>
+        <th>link</th>
+      </tr>
+      {versions.map(([version, packageItem]) => {
+        return <tr key={version} className='version'>
+          <td>{packageItem.version}</td>
+          <td className='list'>
+            {Object.entries(packageItem.vpmDependencies).map(([packageName, version]) => {
+              if (manifests.packages[packageName]) {
+                return <a key={packageName} href={`#${packageName}`}>{packageName}@{version}</a>
+              }
+              return <span key={packageName}>{packageName}@{version}</span>
+            })}
+          </td>
+          <td><a href={packageItem.url}>Download</a></td>
+        </tr>
+      })}
+    </table>
   )
 }
 
