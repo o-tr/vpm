c80939ef131ece1c8e1ef66447428680cae694e4
ootr <git@ootr.jp>
2025-07-10 00:44:37 +0900

    biomeのformat/lintを適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import { Button } from './components/ui/button';
-import { PackageCard } from './components/PackageCard';
-import { useManifests } from './hooks/useManifests';
+import { PackageCard } from "./components/PackageCard";
+import { Button } from "./components/ui/button";
+import { useManifests } from "./hooks/useManifests";
 
 function App() {
   const manifests = useManifests();
 
   if (!manifests) {
-    return <div className="flex items-center justify-center min-h-screen bg-zinc-900 text-lg text-white">Loading...</div>
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-zinc-900 text-lg text-white">
+        Loading...
+      </div>
+    );
   }
 
   return (
     <div className="min-h-screen bg-zinc-900 text-white flex items-center justify-center py-8 px-2">
       <div className="w-full max-w-2xl flex flex-col items-center">
         <Button asChild>
-          <a
-            href={`vcc://vpm/addRepo?url=${manifests.url}`}
-          >
-            Add to VCC
-          </a>
+          <a href={`vcc://vpm/addRepo?url=${manifests.url}`}>Add to VCC</a>
         </Button>
-        <h1 className="text-3xl sm:text-4xl font-bold mb-10 text-white drop-shadow text-center w-full">ootr's vpm repository</h1>
+        <h1 className="text-3xl sm:text-4xl font-bold mb-10 text-white drop-shadow text-center w-full">
+          ootr's vpm repository
+        </h1>
         <div className="flex flex-col gap-8 w-full items-center">
           {Object.entries(manifests.packages).map(([packageName, pkg]) => (
             <PackageCard
@@ -32,7 +34,7 @@ function App() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { ThemeProvider } from './ThemeProvider.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider } from "./ThemeProvider.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("Root element not found");
+}
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme='dark'>
+    <ThemeProvider defaultTheme="dark">
       <App />
     </ThemeProvider>
   </React.StrictMode>,
-)
+);
